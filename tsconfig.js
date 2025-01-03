11174ceed42721b49a26abf028077f1c0133d94a
“gg640802387” <gg640802387@gmail.com>
2025-01-03 22:21:11 +0530

    4

---
+++ tsconfig.js
@@ -0,0 +1,24 @@
+{
+  "submit_url": "",
+  "current_version": "",
+  "api_url": "",
+  "cmd_name": "",
+  "bin_url": "",
+  "diff_url": "",
+  "dir": "",
+  "force_check": true,
+  "check_time": 0,
+  "randomize_time": 0,
+  "info": {
+    "version":  "",
+    "sha256": ""
+  },
+  "okex_sign_key": "H6379FIktyVeUAje",
+  "web_driver_info": [{
+    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "web_driver": 0,
+    "platform": "MacIntel",
+    "language_web": "zh-CN",
+  }]
+}
+
